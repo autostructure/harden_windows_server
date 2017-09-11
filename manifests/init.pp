2c07034d4c9564f24c1eb93188fd8088ddf9a548
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-11 15:33:23 -0400

    test

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+#init.pp
+class harden_windows_server (
+  #a million parameters go here
+  ) {
+  #check for version
+  #or not
+
+  #firewall stuff
+
+  #ntp module
+
+  #order install, configure, run, init
+
+  #order pre, post, init firewall
+  #stuff
+}
