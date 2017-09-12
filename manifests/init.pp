434642bb894a650a0079310a8a210ae2b27f8dc0
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-12 16:48:44 -0400

    added defaults bc no hiera yet

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,16 @@
 #init.pp
 class harden_windows_server (
   #a million parameters go here
-  Boolean $ensure_enforce_password_history_is_set_to_24_or_more_passwords,
-  Boolean $ensure_maximum_password_age_is_set_to_60_or_fewer_days_but_not_0,
-  Boolean $ensure_minimum_password_age_is_set_to_1_or_more_days,
-  Boolean $ensure_minimum_password_length_is_set_to_14_or_more_characters,
-  Boolean $ensure_password_must_meet_complexity_requirements_is_set_to_enabled,
-  Boolean $ensure_store_passwords_using_reversible_encryption_is_set_to_disabled,
-  Boolean $ensure_account_lockout_duration_is_set_to_15_or_more_minutes,
-  Boolean $ensure_account_lockout_threshold_is_set_to_10_or_fewer_invalid_logon_attempts_but_not_0,
-  Boolean $ensure_reset_account_lockout_counter_after_is_set_to_15_or_more_minutes,
+  Boolean $ensure_enforce_password_history_is_set_to_24_or_more_passwords = true,
+  Boolean $ensure_maximum_password_age_is_set_to_60_or_fewer_days_but_not_0 = true,
+  Boolean $ensure_minimum_password_age_is_set_to_1_or_more_days = true,
+  Boolean $ensure_minimum_password_length_is_set_to_14_or_more_characters = true,
+  Boolean $ensure_password_must_meet_complexity_requirements_is_set_to_enabled = true,
+  Boolean $ensure_store_passwords_using_reversible_encryption_is_set_to_disabled = true,
+  Boolean $ensure_account_lockout_duration_is_set_to_15_or_more_minutes = true,
+  Boolean $ensure_account_lockout_threshold_is_set_to_10_or_fewer_invalid_logon_attempts_but_not_0 = true,
+  Boolean $ensure_reset_account_lockout_counter_after_is_set_to_15_or_more_minutes = true,
   ) {
-  #check for version
-  #or not
 
-  #firewall stuff
-
-  #ntp module
-
-  #order install, configure, run, init
-
-  #order pre, post, init firewall
-  #stuff
   include harden_windows_server::configure
 }
