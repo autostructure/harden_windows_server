077739511c2d90480f9447141afa0af581522662
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-21 15:05:33 -0400

    more tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -273,6 +273,213 @@ describe 'harden_windows_server' do
       'policy_value'   => '*S-1-5-32-544'
     )
   }
+  it {
+    should contain_local_security_policy('Lock pages in memory').with(
+      'ensure'         => 'absent'
+    )
+  }
+  # it {
+  #   should contain_local_security_policy('Log on as a batch job').with(
+  #     'ensure'         => 'absent'
+  #   )
+  # }
+  it {
+    should contain_local_security_policy('Manage auditing and security log').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeSecurityPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Modify an object label').with(
+      'ensure'         => 'absent'
+    )
+  }
+  it {
+    should contain_local_security_policy('Modify firmware environment values').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeSystemEnvironmentPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Perform volume maintenance tasks').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeManageVolumePrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Profile single process').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeProfileSingleProcessPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Profile system performance').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeSystemProfilePrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544,*S-1-5-80-3139157870-2983391045-3678747466-658725712-1809340420'
+    )
+  }
+  it {
+    should contain_local_security_policy('Replace a process level token').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeAssignPrimaryTokenPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-19,*S-1-5-20'
+    )
+  }
+  it {
+    should contain_local_security_policy('Restore files and directories').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeRestorePrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Shut down the system').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeShutdownPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Take ownership of files or other objects').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'SeTakeOwnershipPrivilege',
+      'policy_type'    => 'Privilege Rights',
+      'policy_value'   => '*S-1-5-32-544'
+    )
+  }
+  it {
+    should contain_local_security_policy('Accounts: Limit local account use of blank passwords to console logon only').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Control\Lsa\LimitBlankPasswordUse',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Accounts: Rename administrator account').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'NewAdministratorName',
+      'policy_type'    => 'System Access',
+      'policy_value'   => '"Administrator"'
+    )
+  }
+  it {
+    should contain_local_security_policy('Accounts: Rename guest account').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'NewGuestName',
+      'policy_type'    => 'System Access',
+      'policy_value'   => '"Guest"'
+    )
+  }
+  it {
+    should contain_local_security_policy('Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Control\Lsa\SCENoApplyLegacyAuditPolicy',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Audit: Shut down system immediately if unable to log security audits').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Control\Lsa\CrashOnAuditFail',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,0'
+    )
+  }
+  it {
+    should contain_local_security_policy('Devices: Allowed to format and eject removable media').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\AllocateDASD',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '1,"0"'
+    )
+  }
+  it {
+    should contain_local_security_policy('Devices: Prevent users from installing printer drivers').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers\AddPrinterDrivers',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Digitally encrypt or sign secure channel data (always)').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\RequireSignOrSeal',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Digitally encrypt secure channel data (when possible)').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\SealSecureChannel',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Digitally sign secure channel data (when possible)').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\SignSecureChannel',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Disable machine account password changes').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\DisablePasswordChange',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,0'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Maximum machine account password age').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\MaximumPasswordAge',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,30'
+    )
+  }
+  it {
+    should contain_local_security_policy('Domain member: Require strong (Windows 2000 or later) session key').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters\RequireStrongKey',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Interactive logon: Do not display last user name').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System\DontDisplayLastUserName',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,1'
+    )
+  }
+  it {
+    should contain_local_security_policy('Interactive logon: Do not require CTRL+ALT+DEL').with(
+      'ensure'         => 'present',
+      'policy_setting' => 'MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System\DisableCAD',
+      'policy_type'    => 'Registry Values',
+      'policy_value'   => '4,0'
+    )
+  }
 
 
 
