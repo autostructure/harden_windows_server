0bdd91a4b2d21adfb0fde5b6454dda5ae2e67c06
Jack Coleman <jack@autostructure.io>
2017-09-21 10:30:43 -0700

    working tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 describe 'harden_windows_server' do
-  let(:facts) { { :operatingsystem => 'windows' } }
-  it { is_expected.to compile }
+  let(:facts) { {'operatingsystem' => 'windows' } }
+  #it { is_expected.to compile }
+  it {
+    should contain_local_security_policy('Enforce password history').with(
+      'ensure' => 'present'
+    )
+  }
+  it {
+    should contain_local_security_policy('Access this computer from the network').with(
+      'ensure' => 'present'
+    )
+  }
 end
