26f3571869aeafe3a5a46a3f0eb6bcad4725fd73
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-26 10:03:50 -0400

    include RspecPuppetFacts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
+include RspecPuppetFacts
+
 
 #fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
