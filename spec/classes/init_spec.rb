4a7bcc214227fb04c09774a5af865b6e531609fd
juju4 <juju4@users.noreply.github.com>
2018-08-25 18:21:52 -0400

    fix https://ci.appveyor.com/project/juju4/puppet-meta-harden-windows/build/1.1.x.126?fullLog=true#L1201 - spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -44,7 +44,7 @@ describe 'harden_windows_server' do
         'ensure'         => 'present',
         'policy_setting' => 'PasswordComplexity',
         'policy_type'    => 'System Access',
-        'policy_value'   => '1'
+        'policy_value'   => 'enabled'
       )
     }
     it {
@@ -1868,7 +1868,7 @@ describe 'harden_windows_server' do
         'ensure'         => 'present',
         'policy_setting' => 'PasswordComplexity',
         'policy_type'    => 'System Access',
-        'policy_value'   => '1'
+        'policy_value'   => 'enabled'
       )
     }
     it {
