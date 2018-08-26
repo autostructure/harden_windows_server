c92be702989795a7dd5cc72379793bde5e2742be
juju4 <juju4@users.noreply.github.com>
2018-08-26 13:03:06 -0400

    fix typo

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2536,7 +2536,7 @@ describe 'harden_windows_server' do
       )
     }
     it {
-      is_expected.to contain_local_security_policy('Network security: All Local System to use computer identity for NTLM').with(
+      is_expected.to contain_local_security_policy('Network security: Allow Local System to use computer identity for NTLM').with(
         'ensure'         => 'present',
         'policy_setting' => 'MACHINE\System\CurrentControlSet\Control\Lsa\UseMachineId',
         'policy_type'    => 'Registry Values',
